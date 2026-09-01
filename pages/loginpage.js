c3ebe3535edea73c71ae87f0535419f0350c6c4f
YasasriBanuka <banukadias5@gmail.com>
2026-09-01 14:53:31 +0530

    create login page and test file with automated login flow

--- pages/loginpage.js
+++ pages/loginpage.js
@@ -1,24 +1,29 @@
-export class loginpage{
+export class loginpage {
 
     constructor(page) {
-     
-      this.username =  page.locator('#username');
-      this.password = page.locator('#password');
-      this.cbxRememberMe = page.locator('#rememberme');
-      this.btnLogin = page.getByRole('button', { name: 'Log in' });
+        this.cbxRememberMe = page.locator('#nasa_rememberme');
+        this.txtEmailField = page.locator('#nasa_username');
+        this.txtPasswordFiled = page.locator('#nasa_password');
+        this.recaptchaLabelFiled = page.locator('.woocommerce-form-login .g-recaptcha');
+        this.btnLogin = page.getByText('SIGN IN TO YOUR ACCOUNT');
+    }
+      async clickRememberMe() {
+        await this.cbxRememberMe.click();
+    }
+    async enterEmail(email) {
+        await this.txtEmailField.fill(email);
     }
 
-    async enterUsername(username) {
-        await this.username.fill(username);
-    }       
     async enterPassword(password) {
-        await this.password.fill(password);
+        await this.txtPasswordFiled.fill(password);
     }
-    async clickRememberMe() {
-        await this.cbxRememberMe.check();
+
+    async clickRecaptcha() {
+        await this.recaptchaLabelFiled.click();
     }
     async clickLogin() {
         await this.btnLogin.click();
-    }
+    }   
+
 
 }
--- tests/loginpageTest.spec.js
+++ tests/loginpageTest.spec.js
@@ -4,14 +4,18 @@ import { loginpage } from "../pages/loginpage";
 
 test('login_TC_001', async function loginTest({ page }) {
 
+    // call the navigationPage class and navigate to the login page
     let createAccount = new navigationPage(page);
     await createAccount.navigatelogin();
+    await createAccount.clickClosePopup();
     await createAccount.clickUserIcon();
 
+    // TC_001 - Automate the login functionality with valid credentials and verify the user is logged in successfully.
     let login = new loginpage(page);
-    await login.enterUsername('	banukadias5@gmail.com');
-    await login.enterPassword('Banuka@1212');
     await login.clickRememberMe();
+    await login.enterEmail('banukadias5@gmailcom');
+    await login.enterPassword('Banuka@123');
+    await login.clickRecaptcha();
     await login.clickLogin();
 
     await page.waitForTimeout(6000);
