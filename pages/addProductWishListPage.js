b130226e39c91040962b811855e75b9f174b3811
YasasriBanuka <banukadias5@gmail.com>
2026-09-02 12:57:30 +0530

    Automate  product add to cart flow

--- pages/addProductWishListPage.js
+++ pages/addProductWishListPage.js
@@ -1,15 +1,38 @@
 export class addproductWishList {
 
-     constructor(page) {
+    constructor(page) {
         this.btnWishlist = page.locator('a.btn-wishlist[data-prod="12042"]');
-        this.alertMessage = page.getByRole('alert');
+        this.btnviewIcon = page.getByRole('link', { name: 'Quick View' });
+        this.clickbtnImage = page.locator('li.lvfw-product[data-title="Slate Grey"] img');
+        this.btnSize32 = page.locator('li[role="radio"][data-attribute_name="attribute_pa_waist"][data-value="32"]');
+        this.btnPlus = page.locator('a.plus');
+        this.btnAddToCart = page.locator('button.single_add_to_cart_button');
+        this.btnViewCart = page.getByRole('link', { name: 'View cart' });
+
 
     }
-
     async clickWishlist() {
         await this.btnWishlist.click();
     }
+    async clickViewIcon() {
+        await this.btnviewIcon.click();
+    }
+    async clickImage() {
+        await this.clickbtnImage.click();
+    }
+    async clickSize32() {
+        await this.btnSize32.click();
+    }
+    async clickPlus() {
+        await this.btnPlus.dblclick();
+    }
+    async clickAddToCart() {
+        await this.btnAddToCart.click();
+    }
 
+    async clickViewCart() {
+        await this.btnViewCart.click();
+    }
 }
 
 
--- tests/addToCartFlow.spec.js
+++ tests/addToCartFlow.spec.js
@@ -3,21 +3,27 @@ import { navigationPage } from "../pages/navigationPage";
 import { addWishListFlow } from "../pages/addWishListFlowPage";
 import { addproductWishList } from "../pages/addProductWishListPage";
 
-test('Product WishList Workflow_TC_003', async function wishListFLow({page}){
+test('Product WishList Workflow_TC_003', async function addToCartFLow({page}){
 
     let createAccount = new navigationPage(page);
     await createAccount.navigatelogin();
     await createAccount.clickClosePopup();
-  
 
+    // Add Product to WishList-1 
     let addWishList = new addWishListFlow(page);
     await addWishList.clickShorts();
 
-    
+    // add product to wishlist-2 
     let addProductWishList = new addproductWishList(page);
     await addProductWishList.clickWishlist();
+    await page.waitForTimeout(2000);
+    await addProductWishList.clickViewIcon();
+    await addProductWishList.clickImage();
+    await addProductWishList.clickSize32();
+    await addProductWishList.clickPlus();
+    await addProductWishList.clickAddToCart();
+    await addProductWishList.clickViewCart();
 
-
-    await page.waitForTimeout(6000);
+    await page.waitForTimeout(8000);
 
 });
